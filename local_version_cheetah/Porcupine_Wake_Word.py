a341373b16da5b82560d35f78bb9a1cece3a8145
Xinyang Huang <huangxinyang030218@126.com>
2023-07-07 15:11:44 +0800

    BobbyBobby wake word added

--- local_version_cheetah/Porcupine_Wake_Word.py
+++ local_version_cheetah/Porcupine_Wake_Word.py
@@ -4,28 +4,38 @@ import pyaudio
 from AccessKey import ACCESS_KEY
 
 porcupine = pvporcupine.create(
-    access_key = ACCESS_KEY,
-    keywords = ["picovoice", "bumblebee"])
+    access_key=ACCESS_KEY,
+    keyword_paths=['local_version_cheetah/Bobby-Bobby_en_mac_v2_2_0.ppn']
+    )
 
 def get_next_audio_frame():
     myaudio = pyaudio.PyAudio()
     stream = myaudio.open(
         input_device_index=0,
-        rate = porcupine.sample_rate,
-        channels = 1,
-        format = pyaudio.paInt16,
-        input = True,
-        frames_per_buffer = porcupine.frame_length)
-    while True:
-        audio_obj = stream.read(porcupine.frame_length, exception_on_overflow=False)
-        audio_obj_unpacked = struct.unpack_from("h" * porcupine.frame_length, audio_obj)
-        audio_frame = audio_obj_unpacked
+        rate=porcupine.sample_rate,
+        channels=1,
+        format=pyaudio.paInt16,
+        input=True,
+        frames_per_buffer=porcupine.frame_length)
+    try:
+        while True:
+            audio_obj = stream.read(porcupine.frame_length, exception_on_overflow=False)
+            audio_obj_unpacked = struct.unpack_from("h" * porcupine.frame_length, audio_obj)
+            audio_frame = audio_obj_unpacked
 
-        keyword_index = porcupine.process(audio_frame)
-        if keyword_index >= 0:
-            print("Jungle")
+            keyword_index = porcupine.process(audio_frame)
+            if keyword_index >= 0:
+                print("Keyword detected!")
+    except KeyboardInterrupt:
+        print('Interrupted')
+    finally:
+        # Properly clean up the resources
+        stream.stop_stream()
+        stream.close()
+        myaudio.terminate()
 
-get_next_audio_frame()
-porcupine.delete()
 
 
+if __name__ == '__main__':
+    get_next_audio_frame()
+    porcupine.delete()
